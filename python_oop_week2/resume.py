9e198aabeab8ae4ff19cbd53f2f38cb95b8f9e8e
Pitipong-tnk <67319010031@tnk.ac.th>
2024-10-22 16:13:05 +0700

    resume

---
+++ python_oop_week2/resume.py
@@ -0,0 +1,13 @@
+name = input("โปรดกรอกชื่อ\n")
+age = input("โปรดกรออกอายุ\n")
+id = input("โปรดกรอกรหัสประจำตัวนักศึกษา\n")
+yesr = input("โปรดกรอกชั้นปี\n")
+nickname = input("โปรดกรอกชื่อเล่น\n")
+height = float(input("โปรดกรอกส่วนสูง\n"))
+weight = float(input("โปรดกรอกน้ำหนัก\n"))
+sum = height + weight
+print(" ชื่อ: " + name + " อายุ: " + age + " ปี ")
+print(" รหัสประจำตัวนักศึกษา: " + id + " ระดับชั้น: " + yesr)
+print(" ชื่อเล่น: " + nickname)
+print(" ส่วนสูง: " + str(height) + " น้ำหนัก: " + str(weight))
+print(" ส่วนสูง + น้ำหนัก = " + str(sum))
